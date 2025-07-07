2b7d687c64889119f375296c9c0a79582f16b8cf
ASI Saga <10753247+ASISaga@users.noreply.github.com>
2025-07-07 13:31:57 +0530

    Update fetchReddit.js

--- assets/js/fetchReddit.js
+++ assets/js/fetchReddit.js
@@ -1,1 +1,22 @@
+import axios from 'axios'
 
+const token = process.env.ACCESS_TOKEN
+const sub = process.env.SUBREDDIT
+
+async function fetchReddit() {
+  const resp = await axios.get(
+    `https://oauth.reddit.com/r/${sub}/hot?limit=5`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'User-Agent': 'ASI-Saga-Bot/1.0'
+      }
+    }
+  )
+  console.log(JSON.stringify(resp.data, null, 2))
+}
+
+fetchReddit().catch(err => {
+  console.error(err.toString())
+  process.exit(1)
+})
