8d528605b8682124c917822ea04d94a7a97e8511
ASI Saga <10753247+ASISaga@users.noreply.github.com>
2025-07-07 13:41:58 +0530

    Update fetchLinkedIn.js

--- assets/js/fetchLinkedIn.js
+++ assets/js/fetchLinkedIn.js
@@ -1,12 +1,26 @@
 import axios from 'axios'
 
-const token = process.env.ACCESS_TOKEN
-const url = 'https://api.linkedin.com/v2/ugcPosts?q=authors&authors=List(...)'
+// env vars: CLIENT_ID, CLIENT_SECRET, LINKEDIN_REFRESH_TOKEN
+async function getAccessToken() {
+  const resp = await axios.post(
+    'https://www.linkedin.com/oauth/v2/accessToken',
+    null,
+    { params: {
+      grant_type:    'refresh_token',
+      refresh_token: process.env.LINKEDIN_REFRESH_TOKEN,
+      client_id:     process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET
+    }}
+  )
+  return resp.data.access_token
+}
 
 async function fetchLinkedIn() {
-  const resp = await axios.get(url, {
-    headers: { Authorization: `Bearer ${token}` }
-  })
+  const token = await getAccessToken()
+  const resp  = await axios.get(
+    'https://api.linkedin.com/v2/ugcPosts?q=authors&authors=List(...)',
+    { headers: { Authorization: `Bearer ${token}` } }
+  )
   console.log(JSON.stringify(resp.data, null, 2))
 }
 
