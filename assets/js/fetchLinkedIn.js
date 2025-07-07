fd20ed41f1c39f06297dd51c80a73d1901bcf68d
ASI Saga <10753247+ASISaga@users.noreply.github.com>
2025-07-07 13:31:23 +0530

    Update fetchLinkedIn.js

--- assets/js/fetchLinkedIn.js
+++ assets/js/fetchLinkedIn.js
@@ -1,1 +1,16 @@
+import axios from 'axios'
 
+const token = process.env.ACCESS_TOKEN
+const url = 'https://api.linkedin.com/v2/ugcPosts?q=authors&authors=List(...)'
+
+async function fetchLinkedIn() {
+  const resp = await axios.get(url, {
+    headers: { Authorization: `Bearer ${token}` }
+  })
+  console.log(JSON.stringify(resp.data, null, 2))
+}
+
+fetchLinkedIn().catch(err => {
+  console.error(err.toString())
+  process.exit(1)
+})
